e5d23a44a7d6f27f88632472d1d3414ad6e84fd3
Avarfox <Libertacoro@yandex.ru>
2024-07-29 01:29:19 +0300

    architecture first steps

---
+++ src/matrix_h/tests/s21_matrix_test.h
@@ -0,0 +1,14 @@
+/* Copyright [2024] <Casscurs> */
+
+#ifndef S21_MATRIX_TEST_H
+#define S21_MATRIX_TEST_H
+
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "../s21_matrix_oop.h"
+
+#define EPS 1e-6
+
+#endif  // S21_MATRIX_TEST_H
---
+++ src/model/s21_observer.h
@@ -0,0 +1,4 @@
+#ifndef S21_OBSERVER_H
+#define S21_OBSERVER_H
+
+#endif  // S21_OBSERVER_H
